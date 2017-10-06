683606be37460758416168511a774dc66ba530d0
Jerry Fu <fusunstar@hotmail.com>
2017-10-06 08:37:16 +0800

    Add PutData02

---
+++ Controllers/Api/PutData02Controller.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity.Core.Objects;
+using System.Web.Http;
+
+namespace BarCodeApi.Controllers
+{
+    /// <summary>
+    /// BarCode回傳
+    /// </summary>
+    public class PutData02Controller : BaseApiController
+    {
+        /// <summary>
+        /// 上傳盤點資料
+        /// </summary>
+        /// <param name="md">陣列資料</param>
+        /// <returns>
+        /// ReturnInfo JSON ReturnCode=> 0:NO Error/1:異常
+        /// </returns>
+        public ReturnInfo Post([FromBody]PostParam md)
+        {
+            ReturnInfo r = new ReturnInfo();
+            try
+            {
+                string query_from_ip = getUserIP();
+                var json_query = Newtonsoft.Json.JsonConvert.SerializeObject(md);
+                logger.Info("存放資料，IP:{0}， 參數:{1}。", query_from_ip, json_query);
+
+                foreach (var item in md.data)
+                {
+                    ObjectParameter out_value = new ObjectParameter("returnValue01", typeof(int));
+
+                    var i = db.usp_盤點_最後盤點時間_PUT(item.Order_SN_Master, out_value);
+                    var json_detail = Newtonsoft.Json.JsonConvert.SerializeObject(item);
+                    logger.Info("儲存JSON:{0} 回傳值:{1}。", json_detail, out_value.Value);
+                }
+                r.ReturnCode = 0;
+                return r;
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "儲存資料錯誤");
+                r.ReturnCode = ExceptionCode;
+                return r;
+            }
+        }
+        public class PostParam
+        {
+            [Required]
+            public PostModal[] data { get; set; }
+        }
+        public class PostModal
+        {
+            //[Required]
+            public int Order_SN_Master { get; set; }
+        }
+        public class ReturnInfo
+        {
+            public int ReturnCode { get; set; }
+            public int Count { get; set; }
+        }
+    }
+}
--- Model1.Context.cs
+++ Model1.Context.cs
@@ -27,7 +27,15 @@ namespace BarCodeApi
             throw new UnintentionalCodeFirstException();
         }
     
-        public virtual DbSet<VW_盤點廠商_客戶訂單資料> VW_盤點廠商_客戶訂單資料 { get; set; }
+    
+        public virtual int usp_盤點_最後盤點時間_PUT(Nullable<int> p01, ObjectParameter returnValue01)
+        {
+            var p01Parameter = p01.HasValue ?
+                new ObjectParameter("P01", p01) :
+                new ObjectParameter("P01", typeof(int));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction("usp_盤點_最後盤點時間_PUT", p01Parameter, returnValue01);
+        }
     
         public virtual int usp_盤點_資料記錄_PUT(Nullable<int> p01, Nullable<int> p02, string p03, Nullable<float> p04, Nullable<float> p05, ObjectParameter returnValue01)
         {
@@ -52,102 +60,6 @@ namespace BarCodeApi
                 new ObjectParameter("P05", typeof(float));
     
             return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction("usp_盤點_資料記錄_PUT", p01Parameter, p02Parameter, p03Parameter, p04Parameter, p05Parameter, returnValue01);
-        }
-    
-        public virtual int usp_盤點_取得資料11(Nullable<int> p01, Nullable<System.DateTime> p02, string p03, string p04)
-        {
-            var p01Parameter = p01.HasValue ?
-                new ObjectParameter("P01", p01) :
-                new ObjectParameter("P01", typeof(int));
-    
-            var p02Parameter = p02.HasValue ?
-                new ObjectParameter("P02", p02) :
-                new ObjectParameter("P02", typeof(System.DateTime));
-    
-            var p03Parameter = p03 != null ?
-                new ObjectParameter("P03", p03) :
-                new ObjectParameter("P03", typeof(string));
-    
-            var p04Parameter = p04 != null ?
-                new ObjectParameter("P04", p04) :
-                new ObjectParameter("P04", typeof(string));
-    
-            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction("usp_盤點_取得資料11", p01Parameter, p02Parameter, p03Parameter, p04Parameter);
-        }
-    
-        public virtual int usp_盤點_取得資料12(Nullable<int> p01, Nullable<System.DateTime> p02, Nullable<int> p03, string p04)
-        {
-            var p01Parameter = p01.HasValue ?
-                new ObjectParameter("P01", p01) :
-                new ObjectParameter("P01", typeof(int));
-    
-            var p02Parameter = p02.HasValue ?
-                new ObjectParameter("P02", p02) :
-                new ObjectParameter("P02", typeof(System.DateTime));
-    
-            var p03Parameter = p03.HasValue ?
-                new ObjectParameter("P03", p03) :
-                new ObjectParameter("P03", typeof(int));
-    
-            var p04Parameter = p04 != null ?
-                new ObjectParameter("P04", p04) :
-                new ObjectParameter("P04", typeof(string));
-    
-            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction("usp_盤點_取得資料12", p01Parameter, p02Parameter, p03Parameter, p04Parameter);
-        }
-    
-        public virtual int usp_盤點_取得資料13(Nullable<int> p01, Nullable<System.DateTime> p02, string p03, string p04)
-        {
-            var p01Parameter = p01.HasValue ?
-                new ObjectParameter("P01", p01) :
-                new ObjectParameter("P01", typeof(int));
-    
-            var p02Parameter = p02.HasValue ?
-                new ObjectParameter("P02", p02) :
-                new ObjectParameter("P02", typeof(System.DateTime));
-    
-            var p03Parameter = p03 != null ?
-                new ObjectParameter("P03", p03) :
-                new ObjectParameter("P03", typeof(string));
-    
-            var p04Parameter = p04 != null ?
-                new ObjectParameter("P04", p04) :
-                new ObjectParameter("P04", typeof(string));
-    
-            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction("usp_盤點_取得資料13", p01Parameter, p02Parameter, p03Parameter, p04Parameter);
-        }
-    
-        public virtual int usp_盤點_取得資料14(Nullable<int> p01, Nullable<System.DateTime> p02, Nullable<int> p03, Nullable<int> p04, Nullable<int> p05, string p06, string p07)
-        {
-            var p01Parameter = p01.HasValue ?
-                new ObjectParameter("P01", p01) :
-                new ObjectParameter("P01", typeof(int));
-    
-            var p02Parameter = p02.HasValue ?
-                new ObjectParameter("P02", p02) :
-                new ObjectParameter("P02", typeof(System.DateTime));
-    
-            var p03Parameter = p03.HasValue ?
-                new ObjectParameter("P03", p03) :
-                new ObjectParameter("P03", typeof(int));
-    
-            var p04Parameter = p04.HasValue ?
-                new ObjectParameter("P04", p04) :
-                new ObjectParameter("P04", typeof(int));
-    
-            var p05Parameter = p05.HasValue ?
-                new ObjectParameter("P05", p05) :
-                new ObjectParameter("P05", typeof(int));
-    
-            var p06Parameter = p06 != null ?
-                new ObjectParameter("P06", p06) :
-                new ObjectParameter("P06", typeof(string));
-    
-            var p07Parameter = p07 != null ?
-                new ObjectParameter("P07", p07) :
-                new ObjectParameter("P07", typeof(string));
-    
-            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction("usp_盤點_取得資料14", p01Parameter, p02Parameter, p03Parameter, p04Parameter, p05Parameter, p06Parameter, p07Parameter);
         }
     }
 }
